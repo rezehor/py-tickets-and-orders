2db1433859dea971b50679b7aa3c4a9c2d4efd33
Timur Khvat <timurkhvat@gmail.com>
2025-04-21 17:12:15 +0200

    'Solution'

--- services/movie.py
+++ services/movie.py
@@ -31,8 +31,8 @@ def get_movie_by_id(movie_id: int) -> Movie:
 def create_movie(
     movie_title: str,
     movie_description: str,
-    genres_ids: Optional[list[int]] = None,
-    actors_ids: Optional[list[int]] = None,
+    genres_ids: Optional[list[int]],
+    actors_ids: Optional[list[int]],
 ) -> Movie:
     with transaction.atomic():
         movie = Movie.objects.create(
