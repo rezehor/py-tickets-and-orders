8ad3a669e7aa9ff41ea5dfd8d338c762237d9895
Timur Khvat <timurkhvat@gmail.com>
2025-04-21 15:36:32 +0200

    'Solution'

--- db/models.py
+++ db/models.py
@@ -1,8 +1,13 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.exceptions import ValidationError
 from django.db import models
+
+import settings
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=255, unique=True)
+
 
     def __str__(self) -> str:
         return self.name
@@ -22,6 +27,13 @@ class Movie(models.Model):
     actors = models.ManyToManyField(to=Actor, related_name="movies")
     genres = models.ManyToManyField(to=Genre, related_name="movies")
 
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["title"]),
+        ]
+
+
     def __str__(self) -> str:
         return self.title
 
@@ -31,9 +43,11 @@ class CinemaHall(models.Model):
     rows = models.IntegerField()
     seats_in_row = models.IntegerField()
 
+
     @property
     def capacity(self) -> int:
         return self.rows * self.seats_in_row
+
 
     def __str__(self) -> str:
         return self.name
@@ -48,5 +62,63 @@ class MovieSession(models.Model):
         to=Movie, on_delete=models.CASCADE, related_name="movie_sessions"
     )
 
+
     def __str__(self) -> str:
         return f"{self.movie.title} {str(self.show_time)}"
+
+
+class Order(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+
+    class Meta:
+        ordering = ["-created_at"]
+
+
+    def __str__(self) -> str:
+        return f"{self.created_at}"
+
+
+class User(AbstractUser):
+    pass
+
+
+class Ticket(models.Model):
+    movie_session = models.ForeignKey(MovieSession, on_delete=models.CASCADE)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    row = models.IntegerField()
+    seat = models.IntegerField()
+
+
+    def clean(self):
+        if not (1 <= self.row <= self.movie_session.cinema_hall.rows):
+            raise ValidationError(
+                {"row": [f"row number must be in available range: "
+                         f"(1, rows): "
+                         f"(1, {self.movie_session.cinema_hall.rows})"]
+                 })
+        if not (1 <= self.seat <= self.movie_session.cinema_hall.seats_in_row):
+            raise ValidationError(
+                {"seat": [f"seat number must be in available range: "
+                          f"(1, seats_in_row): "
+                          f"(1, "
+                          f"{self.movie_session.cinema_hall.seats_in_row})"]
+                 })
+
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["movie_session", "row", "seat"], name="unique_movie_session_place"),
+        ]
+
+
+    def __str__(self) -> str:
+        return (f"{self.movie_session.movie.title} "
+                f"{self.movie_session.show_time} "
+                f"(row: {self.row}, seat: {self.seat})")
---
+++ services/order.py
@@ -0,0 +1,36 @@
+import datetime
+
+from django.contrib.auth import get_user_model
+from django.db import transaction
+from django.db.models import QuerySet
+
+from db.models import Order, Ticket
+
+
+def create_order(
+        tickets: list,
+        username: str,
+        date: datetime = None,
+) -> None:
+    with transaction.atomic():
+        user = get_user_model().objects.get(username=username)
+        order = Order.objects.create(user=user)
+        if date:
+            order.created_at = date
+        order.save()
+
+        for ticket in tickets:
+            Ticket.objects.create(
+                row=ticket["row"],
+                seat=ticket["seat"],
+                movie_session_id=ticket["movie_session"],
+                order=order
+            )
+
+
+
+def get_orders(username: str = None) -> QuerySet:
+    order = Order.objects.all()
+    if username:
+        order = order.filter(user__username=username)
+    return order
