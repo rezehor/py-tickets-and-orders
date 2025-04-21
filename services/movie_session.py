b82bd39c1beb2c095c213108b41db40673664b67
Timur Khvat <timurkhvat@gmail.com>
2025-04-21 17:14:02 +0200

    'Solution'

--- services/movie_session.py
+++ services/movie_session.py
@@ -6,7 +6,7 @@ from db.models import MovieSession, Ticket
 
 
 def create_movie_session(
-    movie_show_time: str, movie_id: int, cinema_hall_id: int
+    movie_show_time: datetime, movie_id: int, cinema_hall_id: int
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
